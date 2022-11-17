dc1ded29fe1a230f6f99d47d604ca767a8a57bbd
Ismoiljon Abdulloev <110225249+abdulloev19@users.noreply.github.com>
2022-11-17 11:48:46 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const abdulloev = Vue.createApp({
+    data () {
+        return {
+            alltranslations: false,
+            payments: false,
+            perevod1: false,
+            perevod2: false,
+            perevod3: false,
+            perevod4: false,
+            reports: true,
+            filter: true
+        }
+    },
+    methods: {
+
+    },  
+}).mount("#abdulloev")
